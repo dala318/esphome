4666e1bec101cae2f8037b9e73005bd7dbe159aa
Daniel Vikström <daniel@vikstrom.name>
2025-04-02 23:56:36 +0200

    Try go get forward in test

--- tests/dashboard/test_web_server.py
+++ tests/dashboard/test_web_server.py
@@ -75,6 +75,9 @@ async def test_devices_page(dashboard: DashboardTestHelper) -> None:
     assert response.headers["content-type"] == "application/json"
     json_data = json.loads(response.body.decode())
     configured_devices = json_data["configured"]
-    first_device = configured_devices[0]
-    assert first_device["name"] == "pico"
-    assert first_device["configuration"] == "pico.yaml"
+    assert isinstance(configured_devices, list)
+    assert len(configured_devices) != 0
+    if configured_devices:
+        first_device = configured_devices[0]
+        assert first_device["name"] == "pico"
+        assert first_device["configuration"] == "pico.yaml"
