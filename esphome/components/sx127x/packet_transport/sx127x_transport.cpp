0f2fb64c319a3699b75caba00257c3fbb826a1da
Daniel Vikström <daniel@vikstrom.name>
2025-04-02 20:30:40 +0200

    Rebase and use register_listener

--- esphome/components/sx127x/packet_transport/sx127x_transport.cpp
+++ esphome/components/sx127x/packet_transport/sx127x_transport.cpp
@@ -10,18 +10,20 @@ static const char *const TAG = "sx127x_transport";
 
 void SX127xTransport::setup() {
   PacketTransport::setup();
-  if (!this->providers_.empty() || this->is_encrypted_()) {
-    Automation<std::vector<uint8_t>, float, float> *automation_id;
-    automation_id = new Automation<std::vector<uint8_t>, float, float>(this->parent_->get_packet_trigger());
-    LambdaAction<std::vector<uint8_t>, float, float> *lambdaaction_id_2;
-    lambdaaction_id_2 = new LambdaAction<std::vector<uint8_t>, float, float>(
-        [=](std::vector<uint8_t> x, float rssi, float snr) -> void {
-          ESP_LOGD("lambda", "packet %s", format_hex(x).c_str());
-          ESP_LOGD("lambda", "rssi %.2f", rssi);
-          ESP_LOGD("lambda", "snr %.2f", snr);
-        });
-    automation_id_3->add_actions({lambdaaction_id_2});
-  }
+  this->parent_->register_listener(new SX127xTransportListener(this));
+
+  // if (!this->providers_.empty() || this->is_encrypted_()) {
+  //   Automation<std::vector<uint8_t>, float, float> *automation_id;
+  //   automation_id = new Automation<std::vector<uint8_t>, float, float>(this->parent_->get_packet_trigger());
+  //   LambdaAction<std::vector<uint8_t>, float, float> *lambdaaction_id_2;
+  //   lambdaaction_id_2 = new LambdaAction<std::vector<uint8_t>, float, float>(
+  //       [=](std::vector<uint8_t> x, float rssi, float snr) -> void {
+  //         ESP_LOGD("lambda", "packet %s", format_hex(x).c_str());
+  //         ESP_LOGD("lambda", "rssi %.2f", rssi);
+  //         ESP_LOGD("lambda", "snr %.2f", snr);
+  //       });
+  //   automation_id_3->add_actions({lambdaaction_id_2});
+  // }
 }
 
 void SX127xTransport::update() {
--- esphome/components/sx127x/packet_transport/sx127x_transport.h
+++ esphome/components/sx127x/packet_transport/sx127x_transport.h
@@ -9,16 +9,34 @@
 namespace esphome {
 namespace sx127x {
 
+class SX127xTransportListener : public SX127xListener {
+ public:
+  SX127xTransportListener(SX127xTransport *parent) { this->parent_ = parent; }
+  void on_packet(const std::vector<uint8_t> &packet, float rssi, float snr) override {
+    this->parent_->packet_received(packet, rssi, snr);
+  }
+
+ protected:
+  SX127xTransport *parent_;
+};
+
 class SX127xTransport : public packet_transport::PacketTransport, public Parented<SX127x> {
  public:
   void setup() override;
   void update() override;
   float get_setup_priority() const override { return setup_priority::HARDWARE_LATE; }
+  void packet_received(const std::vector<uint8_t> &packet, float rssi, float snr) {
+    ESP_LOGD(TAG, "packet %s", format_hex(packet).c_str());
+    ESP_LOGD(TAG, "rssi %.2f", rssi);
+    ESP_LOGD(TAG, "snr %.2f", snr);
+    std::vector<uint8_t> local_packet(packet);
+    this->process_(local_packet);
+  }
 
  protected:
   void send_packet(std::vector<uint8_t> &buf) const override;
   bool should_send() override { return true; };
-  size_t get_max_packet_size() override { return this->parent_->payload_length_; }
+  size_t get_max_packet_size() override { return 255u; }
   std::vector<uint8_t> receive_buffer_{};
   bool rx_started_{};
   bool rx_control_{};
