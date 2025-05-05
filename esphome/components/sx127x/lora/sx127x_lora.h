558d43ea546dca57eb0005cd23b54f4dbe210a3f
Daniel Vikström <daniel@vikstrom.name>
2025-05-05 13:35:37 +0200

    Tidy up in relations

---
+++ esphome/components/sx127x/lora/sx127x_lora.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/lora/lora.h"
+#include <vector>
+#include "../sx127x.h"
+
+namespace esphome {
+namespace sx127x {
+
+class SX127xLoRa;
+
+class SX127xLoRaListener : public SX127xListener {
+ public:
+  SX127xLoRaListener(SX127xLoRa *parent);
+  void on_packet(const std::vector<uint8_t> &packet, float rssi, float snr) override;
+
+ protected:
+  SX127xLoRa *parent_;
+};
+
+class SX127xLoRa : public lora::LoRa, public Parented<SX127x> {
+ public:
+  void setup() override;
+  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
+  void send_packet(std::vector<uint8_t> &buf) const override { this->parent_->transmit_packet(buf); }
+
+ protected:
+  // size_t get_max_packet_size() override { return 255u; }
+};
+
+}  // namespace sx127x
+}  // namespace esphome
