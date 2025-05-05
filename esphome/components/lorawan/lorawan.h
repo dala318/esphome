6c86515228e4a4a6cc95e1da9ed620ea5ac4e6aa
Daniel Vikström <daniel@vikstrom.name>
2025-05-05 15:25:52 +0200

    Add lorawan files from other repo

--- esphome/components/lorawan/lorawan.h
+++ esphome/components/lorawan/lorawan.h
@@ -3,21 +3,14 @@
 #include "esphome/core/component.h"
 #include "esphome/components/lora/lora.h"
 
+#include "lorawan_crypto.h"
+#include "lorawan_packet.h"
+#include "lorawan_session.h"
+
 #include <vector>
 
 namespace esphome {
 namespace lorawan {
-
-class LoRaWAN;
-
-class LoRaWANListener : public lora::LoRaListener {
- public:
-  LoRaWANListener(LoRaWAN *parent) { this->parent_ = parent; }
-  void on_packet(const std::vector<uint8_t> &packet, float rssi, float snr) override;
-
- protected:
-  LoRaWAN *parent_;
-};
 
 class LoRaWAN : public Component, public Parented<lora::LoRa> {
  public:
@@ -37,9 +30,20 @@ class LoRaWAN : public Component, public Parented<lora::LoRa> {
   }
 
  protected:
+  void send_join_request_();
+  bool process_join_response(const LoRaWANPacket lorawan_packet);
+
   std::array<uint8_t, 16> app_key_;
   std::array<uint8_t, 8> dev_eui_;
   std::array<uint8_t, 8> app_eui_;
+
+  uint16_t dev_nonce_;
+  bool joined_;
+
+  std::unique_ptr<LoRaWANSession> session_;
+  std::shared_ptr<LoRaRadioInterface> radio_;
+
+  unsigned long join_request_time_;
 
   // void call_listeners_(const std::vector<uint8_t> &packet, float rssi, float snr);
   // std::vector<LoRaListener *> listeners_;
---
+++ esphome/components/lorawan/lorawan_session.cpp
@@ -0,0 +1,10 @@
+#include "lorawan_session.h"
+
+namespace esphome {
+namespace lorawan {
+
+// This file can be left empty if we only need to expose the public methods
+// and there is no internal implementation complexity.
+
+}  // namespace lorawan
+}  // namespace esphome
