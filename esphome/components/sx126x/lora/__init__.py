5839fefe789646b0e032c2400c934125e10ced92
Daniel Vikström <daniel@vikstrom.name>
2025-05-13 12:03:12 +0200

    Make optional to specify a full SX12xx-config together with the related LoRa platform component

--- esphome/components/sx126x/lora/__init__.py
+++ esphome/components/sx126x/lora/__init__.py
@@ -1,9 +1,20 @@
 import esphome.codegen as cg
 from esphome.components.lora import LoRa, lora_schema, new_lora
 import esphome.config_validation as cv
+from esphome.const import CONF_ID
 from esphome.cpp_types import Component
 
-from .. import CONF_SX126X_ID, SX126x, SX126xListener, sx126x_modes, sx126x_ns
+from .. import (
+    CONF_MODULATION,
+    CONF_SX126X_ID,
+    CONFIG_SCHEMA as SX126X_CONFIG_SCHEMA,
+    MOD,
+    SX126x,
+    SX126xListener,
+    sx126x_modes,
+    sx126x_ns,
+    to_code as sx126x_to_code,
+)
 
 SX126xLoRa = sx126x_ns.class_("SX126xLoRa", LoRa, Component, SX126xListener)
 
@@ -18,7 +29,7 @@ def validate_lora(config):
     return config
 
 
-CONFIG_SCHEMA = (
+REFERENCE_CONFIG_SCHEMA = (
     lora_schema(SX126xLoRa)
     .extend(
         {
@@ -29,13 +40,38 @@ CONFIG_SCHEMA = (
 )
 
 
-async def to_code(config):
-    # Call the validate_lora function here as well, config validation is done in the
-    # order as defined in config.yaml, so if lora is configured before sx126x, the
-    # sx126x mode will not be set yet.
-    validate_lora(config)
+INCLUDE_CONFIG_SCHEMA = SX126X_CONFIG_SCHEMA.extend(lora_schema(SX126xLoRa)).extend(
+    {
+        cv.GenerateID(CONF_SX126X_ID): cv.declare_id(SX126x),
+        cv.Optional(CONF_MODULATION, default="LORA"): cv.enum({"LORA": MOD["LORA"]}),
+    }
+)
 
-    var = await new_lora(config)
-    sx126x = await cg.get_variable(config[CONF_SX126X_ID])
-    cg.add(var.set_parent(sx126x))
-    cg.add(sx126x.register_listener(var))
+CONFIG_SCHEMA = cv.Any(REFERENCE_CONFIG_SCHEMA, INCLUDE_CONFIG_SCHEMA)
+
+
+async def to_code(config):
+    # LoRa Config with ref to other SX126x is expected to have been provided
+    if CONF_MODULATION not in config:
+        # Call the validate_lora function here as well, config validation is done in the
+        # order as defined in config.yaml, so if lora is configured before sx126x, the
+        # sx126x mode will not be set yet.
+        validate_lora(config)
+
+        var = await new_lora(config)
+        sx126x = await cg.get_variable(config[CONF_SX126X_ID])
+        cg.add(var.set_parent(sx126x))
+        cg.add(sx126x.register_listener(var))
+
+    # LoRa Config with full SX126x module config is provided
+    else:
+        # Create the sx126x component
+        sx126x_config = config.copy()
+        sx126x_config[CONF_ID] = config[CONF_SX126X_ID]
+        await sx126x_to_code(sx126x_config)
+
+        # Create the lora component
+        var = await new_lora(config)
+        sx126x = await cg.get_variable(config[CONF_SX126X_ID])
+        cg.add(var.set_parent(sx126x))
+        cg.add(sx126x.register_listener(var))
--- esphome/components/sx127x/lora/__init__.py
+++ esphome/components/sx127x/lora/__init__.py
@@ -1,9 +1,20 @@
 import esphome.codegen as cg
 from esphome.components.lora import LoRa, lora_schema, new_lora
 import esphome.config_validation as cv
+from esphome.const import CONF_ID
 from esphome.cpp_types import Component
 
-from .. import CONF_SX127X_ID, SX127x, SX127xListener, sx127x_modes, sx127x_ns
+from .. import (
+    CONF_MODULATION,
+    CONF_SX127X_ID,
+    CONFIG_SCHEMA as SX127X_CONFIG_SCHEMA,
+    MOD,
+    SX127x,
+    SX127xListener,
+    sx127x_modes,
+    sx127x_ns,
+    to_code as sx127x_to_code,
+)
 
 SX127xLoRa = sx127x_ns.class_("SX127xLoRa", LoRa, Component, SX127xListener)
 
@@ -18,7 +29,7 @@ def validate_lora(config):
     return config
 
 
-CONFIG_SCHEMA = (
+REFERENCE_CONFIG_SCHEMA = (
     lora_schema(SX127xLoRa)
     .extend(
         {
@@ -28,14 +39,38 @@ CONFIG_SCHEMA = (
     .add_extra(validate_lora)
 )
 
+INCLUDE_CONFIG_SCHEMA = SX127X_CONFIG_SCHEMA.extend(lora_schema(SX127xLoRa)).extend(
+    {
+        cv.GenerateID(CONF_SX127X_ID): cv.declare_id(SX127x),
+        cv.Optional(CONF_MODULATION, default="LORA"): cv.enum({"LORA": MOD["LORA"]}),
+    }
+)
+
+CONFIG_SCHEMA = cv.Any(REFERENCE_CONFIG_SCHEMA, INCLUDE_CONFIG_SCHEMA)
+
 
 async def to_code(config):
-    # Call the validate_lora function here as well, config validation is done in the
-    # order as defined in config.yaml, so if lora is configured before sx127x, the
-    # sx127x mode will not be set yet.
-    validate_lora(config)
+    # LoRa Config with ref to other SX127x is expected to have been provided
+    if CONF_MODULATION not in config:
+        # Call the validate_lora function here as well, config validation is done in the
+        # order as defined in config.yaml, so if lora is configured before sx127x, the
+        # sx127x mode will not be set yet.
+        validate_lora(config)
 
-    var = await new_lora(config)
-    sx127x = await cg.get_variable(config[CONF_SX127X_ID])
-    cg.add(var.set_parent(sx127x))
-    cg.add(sx127x.register_listener(var))
+        var = await new_lora(config)
+        sx127x = await cg.get_variable(config[CONF_SX127X_ID])
+        cg.add(var.set_parent(sx127x))
+        cg.add(sx127x.register_listener(var))
+
+    # LoRa Config with full SX127x module config is provided
+    else:
+        # Create the sx127x component
+        sx127x_config = config.copy()
+        sx127x_config[CONF_ID] = config[CONF_SX127X_ID]
+        await sx127x_to_code(sx127x_config)
+
+        # Create the lora component
+        var = await new_lora(config)
+        sx127x = await cg.get_variable(config[CONF_SX127X_ID])
+        cg.add(var.set_parent(sx127x))
+        cg.add(sx127x.register_listener(var))
