fe37697dfbbfe0f64fac3f171b2b49d98db49497
Daniel Vikström <daniel@vikstrom.name>
2025-06-03 19:48:12 +0200

    Adding radio proxy

--- esphome/components/lmic/__init__.py
+++ esphome/components/lmic/__init__.py
@@ -26,8 +26,8 @@ MULTI_CONF = False  # Static object pointer in callbacks prevent multiple instan
 
 DOMAIN = "lmic"
 
-CONF_LMIC_ID = "lmic_id"
-
+CONF_LORAWAN_VERSION = "lorawan_version"
+CONF_LORAWAN_REGION = "lorawan_region"
 CONF_APP_KEY = "app_key"
 CONF_DEV_EUI = "dev_eui"
 CONF_JOIN_EUI = "join_eui"
@@ -36,6 +36,21 @@ CONF_GEN_APP_KEY = "gen_app_key"
 # CONF_AFTER_JOIN_DELAY = "after_join_delay"
 
 _LOGGER = logging.getLogger(__name__)
+
+LORAWAN_VERSION_TO_FLAG = {
+    "1.0.2": "0x01000200u",
+    "1.0.3": "0x01000300u",
+}
+
+LORAWAN_REGION_TO_FLAG = {
+    "eu868": "CFG_eu868",
+    "us915": "CFG_us915",
+    "au915": "CFG_au915",
+    "as923": "CFG_as923",
+    "as923jp": "CFG_as923jp",
+    "kr920": "CFG_kr920",
+    "in866": "CFG_in866",
+}
 
 CONFIG_SCHEMA = cv.All(
     cv.only_with_arduino,
@@ -55,6 +70,12 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(CONF_GEN_APP_KEY): cv.All(
                 cv.ensure_list(cv.hex_uint8_t), cv.Length(min=16, max=16)
             ),
+            cv.Optional(
+                CONF_LORAWAN_VERSION, default=next(iter(LORAWAN_VERSION_TO_FLAG))
+            ): cv.one_of(*LORAWAN_VERSION_TO_FLAG.keys(), lower=True),
+            cv.Optional(
+                CONF_LORAWAN_REGION, default=next(iter(LORAWAN_REGION_TO_FLAG))
+            ): cv.one_of(*LORAWAN_REGION_TO_FLAG.keys(), lower=True),
             # cv.Optional(
             #     CONF_PERIODICAL_UPLINK_DELAY, default="60s"
             # ): cv.positive_time_period_seconds,
@@ -79,10 +100,18 @@ CONFIG_SCHEMA = cv.All(
 
 
 async def to_code(config: dict[str, Any]):
+    # cg.add_library("mcci-catena/MCCI LoRaWAN LMIC library", "5.0.1")
     cg.add_library(
-        "Arduino-LMIC000", "v5.0.1", "https://github.com/mcci-catena/arduino-lmic"
+        "Arduino-LMIC000",
+        "v5.0.1",
+        "https://github.com/dala318/arduino-lmic#cb5cfde20b0895a333df6e9103a52eba5daf7bd8",
     )
-    # cg.add_build_flag("-DTRANSPARENT_RADIO")
+    cg.add_build_flag(
+        f"-DLMIC_LORAWAN_SPEC_VERSION={LORAWAN_VERSION_TO_FLAG[config[CONF_LORAWAN_VERSION]]}"
+    )
+    cg.add_build_flag("-DARDUINO_LMIC_PROJECT_CONFIG_H_SUPPRESS")
+    cg.add_build_flag(f"-D{LORAWAN_REGION_TO_FLAG[config[CONF_LORAWAN_REGION]]}=1")
+    cg.add_build_flag("-DCFG_external_radio=1")
 
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
--- esphome/components/lmic/lmic_component.cpp
+++ esphome/components/lmic/lmic_component.cpp
@@ -3,7 +3,7 @@
 
 #include "lmic_component.h"
 
-#include "arduino_lmic.h"
+#include <arduino_lmic.h>
 
 // extern "C" {
 // // #include <lbm_lib/lora_basics_modem_version.h>
@@ -18,31 +18,126 @@ namespace lmic {
 
 static const char *const TAG = "lmic";
 
+static osjob_t sendjob;
+static uint8_t mydata[] = "Hello, world!";
+
+void do_send(osjob_t *j) {
+  // Check if there is not a current TX/RX job running
+  if (LMIC.opmode & OP_TXRXPEND) {
+    ESP_LOGI(TAG, "OP_TXRXPEND, not sending");
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
+    ESP_LOGI(TAG, "Packet queued");
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
 void LMICComponent::setup() {
   Component::setup();
-
-  u1_t devStatusAns_battery;
-  LMIC.dataLen = 0;
 
   // Register listener to the LoRa component
   this->parent_->register_listener(this);
 
-  // Set-up the SWL2001 stack
-  // keys_t keys;
-  // std::copy(this->app_key_.begin(), this->app_key_.end(), keys.app_key);
-  // std::copy(this->dev_eui_.begin(), this->dev_eui_.end(), keys.dev_eui);
-  // std::copy(this->join_eui_.begin(), this->join_eui_.end(), keys.join_eui);
-  // std::copy(this->gen_app_key_.begin(), this->gen_app_key_.end(), keys.gen_app_key);
+  // Set-up the LMIC library
+  uint8_t app_key[16];
+  uint8_t dev_eui[8];
+  uint8_t join_eui[8];
+  uint8_t gen_app_key[16];
+
+  std::copy(this->app_key_.begin(), this->app_key_.end(), app_key);
+  std::copy(this->dev_eui_.begin(), this->dev_eui_.end(), dev_eui);
+  std::copy(this->join_eui_.begin(), this->join_eui_.end(), join_eui);
+  std::copy(this->gen_app_key_.begin(), this->gen_app_key_.end(), gen_app_key);
+
   // timings_t timings = {
   //     .join_delay = this->after_join_delay_,
   //     .periodicity = this->periodical_uplink_delay_,
   // };
-  // swl2001_init(this, this->parent_, keys, timings);
+
+  os_init();
+  LMIC_reset();
+  u4_t dev_addr = 0x01234567;
+  // uint8_t nwkskey[sizeof(NWKSKEY)];
+  // uint8_t appskey[sizeof(APPSKEY)];
+  // memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+  // memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+  LMIC_setSession(0x13, dev_addr, gen_app_key, app_key);
+
+  /*
+    #if defined(CFG_eu868)
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set. The LMIC doesn't let you change
+    // the three basic settings, but we show them here.
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+    #elif defined(CFG_us915) || defined(CFG_au915)
+    // NA-US and AU channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    LMIC_selectSubBand(1);
+    #elif defined(CFG_as923)
+    // Set up the channels used in your country. Only two are defined by default,
+    // and they cannot be changed.  Use BAND_CENTI to indicate 1% duty cycle.
+    // LMIC_setupChannel(0, 923200000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+    // LMIC_setupChannel(1, 923400000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+
+    // ... extra definitions for channels 2..n here
+    #elif defined(CFG_kr920)
+    // Set up the channels used in your country. Three are defined by default,
+    // and they cannot be changed. Duty cycle doesn't matter, but is conventionally
+    // BAND_MILLI.
+    // LMIC_setupChannel(0, 922100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+    // LMIC_setupChannel(1, 922300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+    // LMIC_setupChannel(2, 922500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+
+    // ... extra definitions for channels 3..n here.
+    #elif defined(CFG_in866)
+    // Set up the channels used in your country. Three are defined by default,
+    // and they cannot be changed. Duty cycle doesn't matter, but is conventionally
+    // BAND_MILLI.
+    // LMIC_setupChannel(0, 865062500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+    // LMIC_setupChannel(1, 865402500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+    // LMIC_setupChannel(2, 865985000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+
+    // ... extra definitions for channels 3..n here.
+    #else
+    # error Region not supported
+    #endif
+  */
+
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
+
+  // TTN uses SF9 for its RX2 window.
+  LMIC.dn2Dr = DR_SF9;
+
+  // Set data rate and transmit power for uplink
+  LMIC_setDrTxpow(DR_SF7, 14);
+
+  // Start job
+  do_send(&sendjob);
 }
 
-void LMICComponent::loop() {
-  // swl2001_loop();
-}
+void LMICComponent::loop() { os_runloop_once(); }
 
 void LMICComponent::on_packet(const std::vector<uint8_t> &packet, float rssi, float snr) {
   // Handle data from radio
